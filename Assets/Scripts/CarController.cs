5dd165114752df9988665b144964b2c2092ee825
JudeTaylor2 <512447@escg.ac.uk>
2021-03-04 17:40:08 +0000

    completed other car movement tutorial

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,44 +4,82 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float speed;
-    public float turnSpeed;
-    public float gravityMultiplier;
+    public Rigidbody rb;
 
+    public float forwardAccel, reverseAccel, maxSpeed, turnStrength, gravityForce, dragOnGround;
 
-    Rigidbody rb;
+    private float speedInput, turnInput;
+
+    private bool grounded;
+
+    public LayerMask whatIsGround;
+    public float groundRayLength;
+    public Transform groundRayPoint;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb.transform.parent = null;
     }
 
     // Update is called once per frame
+
+    void Update()
+    {
+        speedInput = 0f;
+
+        if(Input.GetAxis("Vertical") > 0 )
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
+       
+        transform.position = rb.transform.position;
+    }
+
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
+        grounded = false;
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
         {
-            rb.AddRelativeForce(Vector3.forward.x, 0, Vector3.forward.z * speed);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddRelativeForce(Vector3.forward.x, 0, Vector3.forward.z * -speed);
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (grounded)
         {
-            rb.AddTorque(Vector3.up * turnSpeed);
+            rb.drag = dragOnGround;
+
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                rb.AddForce(transform.forward * speedInput);
+            }
         }
-        else if (Input.GetKey(KeyCode.A))
+        else
         {
-            rb.AddTorque(-Vector3.up * turnSpeed);
+            rb.drag = 0.1f;
+
+            rb.AddForce(Vector3.up * -gravityForce * 100f);
         }
-
-        rb.AddForce(Vector3.down * gravityMultiplier);
-
-        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
-        localVelocity.x = 0;
-        rb.velocity = transform.TransformDirection(localVelocity);
+        
     }
 }
