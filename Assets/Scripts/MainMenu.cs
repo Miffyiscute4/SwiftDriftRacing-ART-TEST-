861cbbc8bcfb08e8a48db9c60f89b79a3440694a
512477 <512447@chrome.escg.ac.uk>
2021-05-18 12:05:07 +0100

    continued working on main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainPage, levelSelectPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPage.SetActive(true);
+        levelSelectPage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelSelect()
+    {
+        mainPage.SetActive(false);
+        levelSelectPage.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quitting Game");
+    }
+
+    public void LoadTrack(string trackName)
+    {
+        SceneManager.LoadScene(trackName);
+    }
+}
